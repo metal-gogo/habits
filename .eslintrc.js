613011b8dbb93cb80dea916e7953dc09e8209acc
Daniel González <d.g.g69@me.com>
2020-08-01 18:44:35 -0500

    :wrench: seting up eslint, stylelint, and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ["react-app", "airbnb", "airbnb/hooks"],
+  rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  }
+};
---
+++ .stylelintrc.js
@@ -0,0 +1,21 @@
+const word = "[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*";
+const block = word;
+const element = `(?:__${word})?`;
+const modifier = `(?:--${word})?`;
+const bemPattern = `^\.${block}${element}${modifier}$`;
+
+module.exports = {
+  extends: [
+    "stylelint-config-sass-guidelines",
+    "stylelint-config-prettier"
+  ],
+  rules: {
+    "selector-class-pattern": bemPattern,
+    "selector-no-qualifying-type": [
+      true, {
+        "ignore": [ "attribute" ]
+      }
+    ],
+    "string-quotes": "double"
+  }
+}
