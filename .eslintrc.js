b8b339dd3d092ad9d5bcf9aa96bba343e4743366
Daniel González <d.g.g69@me.com>
2020-08-05 16:23:03 -0500

    :wrench: remove import/prefer-default-export rule from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ["react-app", "airbnb", "airbnb/hooks"],
   rules: {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "import/prefer-default-export": "off",
   }
 };
