16b888231a6b192676992493214b34c9df96d0bf
Daniel González <d.g.g69@me.com>
2020-08-14 21:52:27 -0500

    :sparkles: create NotFound page

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="not-found page">
+    <h2>We could not find the page you are looking for...</h2>
+  </div>
+);
+
+export default NotFound;
