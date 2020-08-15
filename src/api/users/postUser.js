b07f96fc583f4f15b15eecceb72b084e319a3f71
Daniel González <d.g.g69@me.com>
2020-08-14 21:46:07 -0500

    :sparkles: create postUser on users API

---
+++ src/api/users/postUser.js
@@ -0,0 +1,21 @@
+import { firebaseDB } from 'dependencies/firebase';
+
+import collectionKeys from 'api/collectionKeys';
+
+import composeUserData from './utils/composeUserData';
+
+const postUser = async (userId, firebaseUser) => {
+  try {
+    const newUser = composeUserData(firebaseUser);
+    await firebaseDB
+      .collection(collectionKeys.users)
+      .doc(userId)
+      .set(newUser);
+    return newUser;
+  } catch (error) {
+    console.error('Error saving user document: ', error);
+  }
+  return null;
+};
+
+export default postUser;
