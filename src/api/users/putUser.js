3fb735b32a3b032fcb1fcd11c3056728d1e5ed64
Daniel González <d.g.g69@me.com>
2020-08-14 21:47:04 -0500

    :sparkles: create putUser on users API

---
+++ src/api/users/putUser.js
@@ -0,0 +1,21 @@
+import { firebaseDB } from 'dependencies/firebase';
+
+import collectionKeys from 'api/collectionKeys';
+
+import composeUserData from './utils/composeUserData';
+
+const putUser = async (userId, firebaseUser) => {
+  try {
+    const user = composeUserData(firebaseUser);
+    await firebaseDB
+      .collection(collectionKeys.users)
+      .doc(userId)
+      .update(user);
+    return user;
+  } catch (error) {
+    console.error('Error saving user document: ', error);
+  }
+  return null;
+};
+
+export default putUser;
