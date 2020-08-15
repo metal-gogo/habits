03c94de338239da3edd3ed6feaae236ca652940b
Daniel González <d.g.g69@me.com>
2020-08-15 09:14:24 -0500

    :recycle: rename argument to provider in composeProviderData

--- src/api/users/utils/composeProviderData.js
+++ src/api/users/utils/composeProviderData.js
@@ -1,12 +1,12 @@
 import removeEmptyOrNilValues from 'utils/removeEmptyOrNilValues';
 
-const composeProviderData = (firebaseUserProvider) => removeEmptyOrNilValues({
-  displayName: firebaseUserProvider.displayName,
-  email: firebaseUserProvider.email,
-  phoneNumber: firebaseUserProvider.phoneNumber,
-  photoURL: firebaseUserProvider.photoURL,
-  providerType: firebaseUserProvider.providerId,
-  uid: firebaseUserProvider.uid,
+const composeProviderData = (provider) => removeEmptyOrNilValues({
+  displayName: provider.displayName,
+  email: provider.email,
+  phoneNumber: provider.phoneNumber,
+  photoURL: provider.photoURL,
+  providerType: provider.providerId,
+  uid: provider.uid,
 });
 
 export default composeProviderData;
