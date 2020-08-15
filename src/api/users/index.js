4cb4eadd0110d9782a11fa5a9530d31cfdb3b56d
Daniel González <d.g.g69@me.com>
2020-08-14 21:47:39 -0500

    :sparkles: update auth.js to save/update user data

---
+++ src/api/users/index.js
@@ -0,0 +1,9 @@
+import getUser from './getUser';
+import postUser from './postUser';
+import putUser from './putUser';
+
+export {
+  getUser,
+  putUser,
+  postUser,
+};
--- src/contexts/auth/auth.js
+++ src/contexts/auth/auth.js
@@ -1,18 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { firebaseAuth } from 'dependencies/firebase';
 
+import * as UsersApi from 'api/users';
+
 const AuthContext = React.createContext();
 
 const AuthProvider = (props) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const unregisterAuthObserver = firebaseAuth().onAuthStateChanged((firebaseUser) => {
-      setUser(firebaseUser);
+    const unregisterAuthObserver = firebaseAuth().onAuthStateChanged(async (firebaseUser) => {
+      try {
+        let applicationUser = null;
+        if (firebaseUser) {
+          const userId = firebaseUser.uid;
+          const dbUser = await UsersApi.getUser(userId);
+          if (dbUser) {
+            applicationUser = await UsersApi.putUser(userId, firebaseUser);
+          } else {
+            applicationUser = await UsersApi.postUser(userId, firebaseUser);
+          }
+        }
+        setUser(applicationUser);
+      } catch (e) {
+        setUser(null);
+      }
     });
 
     return unregisterAuthObserver;
-  });
+  }, []);
 
   return (
     <AuthContext.Provider
