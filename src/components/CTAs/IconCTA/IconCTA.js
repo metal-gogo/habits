e5285eb0605895cd6ceab0827b047874e8f35be8
Daniel González <d.g.g69@me.com>
2020-08-24 16:16:52 -0500

    ✨ added close and menu icons to IconCTA

--- src/components/CTAs/IconCTA/IconCTA.js
+++ src/components/CTAs/IconCTA/IconCTA.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 import './IconCTA.scss';
 
 import { ReactComponent as IconExit } from 'icons/exit.svg';
+import { ReactComponent as IconMenu } from 'icons/menu.svg';
+import { ReactComponent as IconClose } from 'icons/close.svg';
 import { ReactComponent as IconTrash } from 'icons/trash.svg';
 
-export const ICON_TYPES = ['exit', 'trash'];
+export const ICON_TYPES = ['close', 'exit', 'menu', 'trash'];
 
 /* eslint-disable react/jsx-props-no-spreading */
 const IconCTA = ({
@@ -14,7 +16,9 @@ const IconCTA = ({
 }) => {
   const renderIcon = () => {
     const iconsMap = {
+      close: <IconClose className="icon-cta__icon" />,
       exit: <IconExit className="icon-cta__icon" />,
+      menu: <IconMenu className="icon-cta__icon" />,
       trash: <IconTrash className="icon-cta__icon" />,
     };
 
