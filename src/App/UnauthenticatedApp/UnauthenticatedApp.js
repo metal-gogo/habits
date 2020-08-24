d4f30bbb25a299a97c5e460937613b41dba2318b
Daniel González <d.g.g69@me.com>
2020-08-24 17:17:42 -0500

    ✨ make UnauthenticatedApp 100VH

--- src/App/UnauthenticatedApp/UnauthenticatedApp.js
+++ src/App/UnauthenticatedApp/UnauthenticatedApp.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Router from 'router/Router';
 
+import setVH100 from 'utils/setVH100/setVH100';
+
+import './UnauthenticatedApp.scss';
+
 import Home from 'pages/Home';
 
-const UnauthenticatedApp = () => (
-  <section className="unauthenticated-app">
-    <Router hasFadeTransition>
-      <Home default />
-    </Router>
-  </section>
-);
+const UnauthenticatedApp = () => {
+  useEffect(() => {
+    setVH100();
+  }, []);
+
+  return (
+    <section className="unauthenticated-app">
+      <Router hasFadeTransition className="unauthenticated-app__router">
+        <Home default />
+      </Router>
+    </section>
+  );
+};
 
 export default UnauthenticatedApp;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { StyledFirebaseAuth } from 'dependencies/react-firebaseui';
 
-import setVH100 from 'utils/setVH100/setVH100';
-
 import './Home.scss';
 
-const Home = () => {
-  useEffect(() => {
-    setVH100();
-  }, []);
-
-  return (
-    <section className="page home">
-      <h1>Habits</h1>
-      <StyledFirebaseAuth />
-      <p>Make commitements to create great habits.</p>
-    </section>
-  );
-};
+const Home = () => (
+  <section className="page home">
+    <h1>Habits</h1>
+    <StyledFirebaseAuth />
+    <p>Make commitements to create great habits.</p>
+  </section>
+);
 
 export default Home;
